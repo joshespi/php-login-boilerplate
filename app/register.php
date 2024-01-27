223b78ec49df07a0a45396210618121962e81b2c
Josh Espinoza <joshespi@gmail.com>
2024-01-27 09:20:52 -0700

    simple registration logic

--- app/register.php
+++ app/register.php
@@ -2,6 +2,22 @@
 <?php
 $pageName = "Register";
 include_once(__DIR__ . "/includes/dbConnect.php");
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+        $result = mysqli_query($db, $sql);
+        if ($result) {
+            echo "User created successfully.";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($db);
+        }
+    }
+
+}
 include_once(__DIR__ . "/includes/header.php");
 ?>
 <h2>Registration Form</h2>
