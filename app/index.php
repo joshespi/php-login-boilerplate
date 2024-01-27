cee46526b05f54505afec13660cf85dcd87704e4
Josh Espinoza <joshespi@gmail.com>
2024-01-27 09:26:21 -0700

    simple login form

--- app/index.php
+++ app/index.php
@@ -2,6 +2,15 @@
 $pageName = "Logins";
 include_once(__DIR__ . "/includes/header.php");
 ?>
+<h2>Login Form</h2>
+
+<form action="index.php" method="post">
+    <label for="username">Username:</label><br>
+    <input type="text" id="username" name="username"><br>
+    <label for="password">Password:</label><br>
+    <input type="password" id="password" name="password">
+    <input type="submit" value="Submit">
+</form> 
 <a href="/register.php">Register</a>
 <?php
 include_once(__DIR__ . "/includes/footer.php");
