1083f15df4de2bee1accbfaff1f568093dea723e
Josh Espinoza <joshespi@gmail.com>
2024-01-27 08:22:13 -0700

    made basic document structures

---
+++ app/includes/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espi's PHP Login/Registration Boilerplate</title>
+</head>
+<body>
