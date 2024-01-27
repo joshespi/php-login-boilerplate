08adc57fa67e8bb3407b72aa7eee905ab0159fc1
Josh Espinoza <joshespi@gmail.com>
2024-01-27 09:26:50 -0700

    simple login logic

--- app/index.php
+++ app/index.php
@@ -3,6 +3,34 @@ $pageName = "Logins";
 session_start();
 
 include_once(__DIR__ . "/includes/dbConnect.php");
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (isset($_POST['username']) && isset($_POST['password'])) {
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM users WHERE username = '$username'";
+        $result = mysqli_query($db, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            $user = mysqli_fetch_assoc($result);
+            if (password_verify($password, $user['password'])) {
+                // echo "Login successful.";
+                // Here you can set the user data in the session to keep the user logged in
+                $_SESSION['username'] = $username;
+                // Then redirect to the desired page
+                header('Location: dashboard.php');
+            } else {
+                echo "Invalid password.";
+            }
+        } else {
+            echo "Invalid username.";
+        }
+    }
+}
+
+
+
+
 
 include_once(__DIR__ . "/includes/header.php");
 ?>
